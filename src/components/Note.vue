f2065a348ea0eee3f9ca7ebd5672858777a2c948
Mustapha Adubiagbe <jidemustapha2013@gmail.com>
2018-10-25 23:42:29 -0500

    setup

---
+++ src/components/Note.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="note">
+        note
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="sidebar">
+        <div class="sidebar__content">
+            Boo, no notes. Get writing!
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+  .sidebar {
+    background-color: #F9F9F9;
+    flex: 1;
+    height: 100%;
+    min-width: 28%;
+
+    &__content {
+        padding: 30px;
+
+        a {
+            text-decoration: none;
+            color: inherit;
+        }
+    }
+  }
+</style>
